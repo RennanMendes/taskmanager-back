bc87a9c8726e4ffb19a7fcb6fd0e52585a639d77
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2024-10-27 14:39:59 -0300

    feat: adiciona senha para conexão redis

--- src/main/java/br/com/abreu/taskmanager/infra/config/RedisConfig.java
+++ src/main/java/br/com/abreu/taskmanager/infra/config/RedisConfig.java
@@ -1,17 +1,23 @@
 package br.com.abreu.taskmanager.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
+
+    @Value("${spring.redis.password}")
+    private String password;
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
@@ -24,6 +30,13 @@ public class RedisConfig {
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(cacheConfiguration())
                 .build();
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
     }
 
 }
