3cbbc6e35e408c15b01a1d767bbad9d04de23370
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2024-10-27 00:02:36 -0300

    fix: redis config

--- src/main/java/br/com/abreu/taskmanager/infra/config/RedisConfig.java
+++ src/main/java/br/com/abreu/taskmanager/infra/config/RedisConfig.java
@@ -1,18 +1,13 @@
 package br.com.abreu.taskmanager.infra.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+
 import java.time.Duration;
 
 @Configuration
@@ -20,25 +15,16 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory,
-                                     @Value("${cache.expiration}") Long duration) {
-        RedisCacheConfiguration redisCacheConfiguration = redisCacheConfiguration(duration);
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(2)); // Configura o TTL para 2 minutos
     }
 
-    private RedisCacheConfiguration redisCacheConfiguration(Long duration) {
-        ObjectMapper objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
-                .disableKeyPrefix()
-                .entryTtl(Duration.ofSeconds(duration));
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration())
+                .build();
     }
 
 }
